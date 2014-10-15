439ef0a7ea64a1a71cc8658bd46a1a5c8af6d2bc
Nami Kinjo <n14004@std.it-college.ac.jp>
2014-10-15 11:54:46 +0900

    Ex2演習_141014

--- Ex2.java
+++ Ex2.java
@@ -15,8 +15,41 @@ class Ex2_0 extends Ex{
 
 		System.out.println("xの値は" + x + "です。");
 		System.out.println("yの値は" + y + "です。");
-		System.out.println("合計は" + (x + y) + "です。");
-		System.out.println("平均は" + (x + y) / 2 + "です。");
+		System.out.println("合計は" + (x + y ) + "です。");
+		System.out.println("平均は" + (x + y ) / 2 + "です。");
+	}
+}
+
+class Ex2_1 extends Ex{
+	void execute(){
+		double x;
+		double y;
+
+		x = 63;
+		y = 18;
+
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+		System.out.println("合計は" + (x + y ) + "です。");
+		System.out.println("平均は" + (x + y ) / 2 + "です。");
+	}
+}
+
+class Ex2_2 extends Ex{
+	void execute(){
+		int x;
+		int y;
+        int z;
+
+		x = 63;
+		y = 18;
+        z = 21;
+
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+        System.out.println("zの値は" + z + "です。");
+		System.out.println("合計は" + (x + y + z) + "です。");
+		System.out.println("平均は" + (x + y + z) / 3 + "です。");
 	}
 }
 
@@ -24,6 +57,9 @@ public class Ex2 {
 	public static void main(String[] args){
 		Ex a;
 		a = new Ex2_0();
+        a.execute();
+        a = new Ex2_1();
+        a.execute();
+        a = new Ex2_2();
 		a.execute();
-	}
-}
+	} }
