ca3096a4b30bf605d4d60301655711f8bcfbe72e
Nami Kinjo <n14004@std.it-college.ac.jp>
2014-11-11 10:08:25 +0900

    change

--- Ex3.java
+++ Ex3.java
@@ -1,45 +1,53 @@
+public class Ex3{
+    public static void main(String[] args){
+    Ex3 ex3 = new Ex3;
+    }
+}
 class Ex3{
 
-	public String ex3_0(int n){
+    public String ex3_0(int n){
+        if(n > 0){
+            return "その値は正です";
+        }else{
+            return "その値は０か負です";
+        }
 
-		return "dummy";
+    }
 
-	}
+    public int ex3_1(int n){
 
-	public int ex3_1(int n){
-		
-		return -1;
+        return -1;
 
-	}
+    }
 
-	public String ex3_2(int a, int b){
+    public String ex3_2(int a, int b){
 
-		return "dummy";
-	}
+        return "dummy";
+    }
 
-	public String ex3_3(int a, int b){
+    public String ex3_3(int a, int b){
 
-		return "dummy";
+        return "dummy";
 
-	}
+    }
 
-	public String ex3_4(int a){
+    public String ex3_4(int a){
 
-		return "dummy";
+        return "dummy";
 
-	}
+    }
 
-	public String ex3_5(int a){
+    public String ex3_5(int a){
 
-		return "dummy";
-	}
-	
-	public String ex3_6(int a){
-		return "dummy";
-	}
+        return "dummy";
+    }
 
-	public String ex3_7(int a){
-		return "dummy";
-	}
+    public String ex3_6(int a){
+        return "dummy";
+    }
+
+    public String ex3_7(int a){
+        return "dummy";
+    }
 }
 
